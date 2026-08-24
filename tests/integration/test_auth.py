16c8cc3fa1eda79d68d78276fbb1313b36935cce
Maaike <mlimper@wmo.int>
2026-08-24 15:19:22 +0200

    decode path (#20)
    * decode path
    
    * use while-loop
    
    * Update __init__.py
    
    * attempt to add integration test
    
    * test 401 on TOPIC3 and TOPIC5
    
    * fix
    
    * test
    
    * post wrong_token
    
    * flake8
    
    ---------
    
    Co-authored-by: Tom Kralidis <tomkralidis@gmail.com>

--- tests/integration/test_auth.py
+++ tests/integration/test_auth.py
@@ -108,6 +108,23 @@ def test_header_auth():
     r = requests.get(URL + '/authorize', headers=headers)
     assert r.status_code == 200
 
+    data = {'topic': TOPIC3, 'token': TOKEN3}
+    r = requests.post(URL + '/add_token', data=data)
+    assert r.status_code == 200
+
+    headers = {
+            'X-Original-URI': f'/{TOPIC3}',
+            'Authorization': f'Bearer {TOKEN1}',
+            'X-Api-Http-Method': 'POST'
+    }
+    r = requests.get(URL + '/authorize', headers=headers)
+    assert r.status_code == 401
+
+    TOPIC3_ALT = TOPIC3.replace('collections', '%63ollections')
+    headers['X-Original-URI'] = f'/{TOPIC3_ALT}'
+    r = requests.get(URL + '/authorize', headers=headers)
+    assert r.status_code == 401
+
     headers = {
         'X-Original-URI': f'/{TOPIC}',
         'Authorization': f'Bearer {TOKEN}',
