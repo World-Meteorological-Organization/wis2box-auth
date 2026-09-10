5a15ec69b85349d9405af368735a04a4da385e27
Maaike <mlimper@wmo.int>
2026-09-10 12:39:18 +0200

    fix permission error and add health-check (#21)
    * set /data/wis2box as home-dir and ensure tests-docker uses docker
    
    * add health-check, fix docker-test
    
    * fix docker test
    
    * add health check to docker test
    
    * sleep 3 seconds before health-check
    
    * change topics to items
    
    * Update app.py
    
    ---------
    
    Co-authored-by: Tom Kralidis <tomkralidis@gmail.com>

--- wis2box_auth/app.py
+++ wis2box_auth/app.py
@@ -29,7 +29,8 @@ from wis2box_auth import (
     is_resource_open,
     create_token,
     delete_token,
-    extract_topic
+    extract_topic,
+    topics
 )
 from wis2box_auth.log import setup_logger
 
@@ -53,6 +54,14 @@ def get_response(code: int, description: str) -> Tuple[dict, int]:
     """
 
     return {'code': code, 'description': description}, code
+
+
+@app.route('/status')
+def status():
+    """Container status endpoint with ACL topic count."""
+
+    topic_count = len(topics())
+    return f'{topic_count} items in access-control list', 200
 
 
 @app.route('/authorize')
